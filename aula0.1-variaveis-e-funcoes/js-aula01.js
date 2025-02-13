76f70719581d8820b51a4d2a693a0f890a53195f
MauricioRVeiga <mauricio.rgv15@gmail.com>
2025-02-12 21:28:48 -0300

    primeiro arquivo js

---
+++ aula0.1-variaveis-e-funcoes/js-aula01.js
@@ -0,0 +1,17 @@
+// comentario em linha
+/* comentario
+em bloco */
+
+
+// Declaração de variaveis no Js
+
+const nome = "Mauricio" // Uma constante precisa de um valor
+let  Nome // Let permite iniciar a variavel vazia
+
+var cor = "Azul"
+var cor = "Amarelo"
+let cidade = "Registro"
+cidade = "Jacupiranga"
+
+const message = "Hello, World ! Iniciando em JavaScript"
+console.log(message)
